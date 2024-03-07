d4f691d84b47e71697ddd74baf7ceb8e4d83edd8
Monk (looking for PhD Fall’24) <aadityaura@gmail.com>
2024-03-07 23:58:37 +0530

    Update lora.py

--- layers/lora.py
+++ layers/lora.py
@@ -1,4 +1,9 @@
+#inspired from https://magazine.sebastianraschka.com/p/lora-and-dora-from-scratch
+
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
 import torch.nn as nn
+import torch
 
 class LoRALayer(nn.Module):
     def __init__(self, in_dim, out_dim, rank, alpha):
@@ -23,3 +28,18 @@ class LinearWithLoRA(nn.Module):
 
     def forward(self, x):
         return self.linear(x) + self.lora(x)
+
+
+# This LoRA code is equivalent to LinearWithLoRA
+class LinearWithLoRAMerged(nn.Module):
+    def __init__(self, linear, rank, alpha):
+        super().__init__()
+        self.linear = linear
+        self.lora = LoRALayer(
+            linear.in_features, linear.out_features, rank, alpha
+        )
+
+    def forward(self, x):
+        lora = self.lora.A @ self.lora.B
+        combined_weight = self.linear.weight + self.lora.alpha*lora.T
+        return F.linear(x, combined_weight, self.linear.bias)
